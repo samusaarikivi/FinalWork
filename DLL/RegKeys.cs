bcefb91582d71d0bb05c113904644c4fe5f5b304
Saarsamu <Samu.saarikivi@gmail.com>
2016-11-29 22:31:52 +0200

    Project started

---
+++ DLL/RegKeys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public class RegKeys
+    {
+        static string names = "Names";
+        public static void Create()
+        {
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(names);
+            key.SetValue("Name", "Samu");
+            key.Close();
+        }
+
+        public static void Read(string name)
+        {
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(names);
+            Console.WriteLine(key.GetValue(name));
+            key.Close();
+        }
+    }
+}
---
+++ FinalWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLL;
+
+namespace FinalWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegKeys.Create();
+            RegKeys.Read("Name");
+            switch (args[0])
+            {
+                case "-r":
+                    RegKeys.Read(args[1]);
+                    break;
+            }
+        }
+    }
+}
